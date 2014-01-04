70759063e5e96a0f8293f4f360def22fb5b1745f
Sergio Alvarez-Napagao <salvarez@lsi.upc.edu>
2014-01-04 04:34:48 +0100

    configuration for eclipse importing

--- project.clj
+++ project.clj
@@ -8,5 +8,6 @@
                  [jivesoftware/smack "3.1.0"]
                  [jivesoftware/smackx "3.1.0"]]
   :profiles {:uberjar {:aot :all}}
+  :plugins [[no-man-is-an-island/lein-eclipse "2.0.0"]]
   :target-path "target/%s/"
   :main tailrecursion.hiprepl)
